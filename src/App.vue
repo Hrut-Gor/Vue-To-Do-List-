9a17da9da2c92c3a25b525c77479ef53419535b3
hrut gor <hrutgor2000@gmail.com>
2023-04-28 14:28:12 +0530

    setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template lang="">
+  <div class="border border-red-500 !text-red-600 text-xl text-center m-[20px]">
+    <h1 class=" text-blue-400">
+      Hello world!
+    </h1>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="">
+  
+</style>
